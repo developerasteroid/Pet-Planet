25782766a3e44f64d470821c4974dbba1514820a
suman <developerasteroid@gmail.com>
2024-04-05 20:53:47 +0530

    Added  First Route in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import './App.css'
+import SellerRegister from './seller/SellerRegister'
 // import Admin from './admin/Admin'
 import Seller from './seller/Seller'
 
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom'
+
 function App() {
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <>
+      <Route path='/' element={<Seller/>}/>
+      <Route path='/signup' element={<SellerRegister/>}/>
+      </>
+    )
+  )
+
   return (
     <>
-      <Seller/>
+      <RouterProvider router={router}/>
       {/* <Admin/> */}
     </>
   )
